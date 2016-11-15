b88a87f46d0bb8c9156dffa83efc7c7cffb5972d
fffuture <t30t40t50@gmail.com>
2016-11-15 23:30:10 +0200

    index page : bug

--- src/limnal.cr
+++ src/limnal.cr
@@ -1,5 +1,10 @@
 require "./limnal/*"
+require "kemal"
 
 module Limnal
-  # TODO Put your code here
+  get "/" do |env|
+    send_file env, "public/index.html", "text/html"
+  end
 end
+
+Kemal.run
