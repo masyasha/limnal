5956caf01755a94cde99dcb1e26959cf1650fea0
fffuture <t30t40t50@gmail.com>
2016-11-16 00:24:36 +0200

    ecr layout

--- src/limnal.cr
+++ src/limnal.cr
@@ -3,7 +3,8 @@ require "kemal"
 
 module Limnal
   get "/" do |env|
-    send_file env, "public/index.html", "text/html"
+    #send_file env, "public/index.html", "text/html"
+    render "public/index.ecr", "public/1.ecr"
   end
 end
 
