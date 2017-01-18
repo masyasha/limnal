4a06cea717ea45fa90a7510eba4c9a1d40856bf7
mike <t30t40t50@gmail.com>
2017-01-18 16:29:55 +0200

    nazi fallin slowly

--- main.py
+++ main.py
@@ -1,25 +1,46 @@
 import sys, pygame
 pygame.init()
 
-size = width, height = 320, 240
-speed = [2, 2]
-black = 0, 0, 0
+size = width, height = 400, 700
+speed = 10
+black = 255, 255, 255
 
 screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("ball.gif")
+clock = pygame.time.Clock()
+ball = pygame.image.load("tot2.png")
+ball = pygame.transform.scale(ball, (40, 40))
 ballrect = ball.get_rect()
+ballrect.x = int(width/2)
+
+face_direction = 'right'
+
+def dispatch_press(e, false=None):
+    if e.key == pygame.K_LEFT and false != 'l':
+        return (-speed, 1)
+    if e.key == pygame.K_RIGHT and false != 'r':
+        return (speed, 1)
+    return (0, 1)
 
 while 1:
+    move = (0,1)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if ballrect.left < 0:
+                move = dispatch_press(event, 'l')
+            elif ballrect.right > width:
+                move = dispatch_press(event, 'r')
+            else:
+                move = dispatch_press(event)
+    if move[0] > 0 and face_direction != 'right':
+        ball = pygame.transform.flip(ball, True, False)
+        face_direction = 'right'
+    if move[0] < 0 and face_direction != 'left':
+        ball = pygame.transform.flip(ball, True, False)
+        face_direction = 'left'
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
+    ballrect = ballrect.move(move)
     screen.fill(black)
     screen.blit(ball, ballrect)
     pygame.display.flip()
+    clock.tick(40)
